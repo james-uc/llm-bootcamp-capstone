c11fff37e148c885b67e1348c3c1c8b835acb328
James Wang <james@userclouds.com>
2024-12-16 09:22:30 -0800

    basic chainlit scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# llm-bootcamp-capstone
+# Scientific Paper Assistant
+
+## Description
+
+This is an AI assistant that ingests scientific papers and allows a user to interact with and query it in order to better understand the latest developments in a particular area.
---
+++ app.py
@@ -0,0 +1,86 @@
+import chainlit as cl
+import openai
+import os
+import base64
+from langsmith.wrappers import wrap_openai
+from langsmith import traceable
+
+config = {
+    "api_key": os.getenv("OPENAI_API_KEY"),
+    "endpoint_url": "https://api.openai.com/v1",
+}
+
+model_kwargs = {
+    "model": "gpt-4o",
+    "temperature": 1.2,
+    "max_tokens": 500,
+}
+
+client = wrap_openai(
+    openai.AsyncClient(api_key=config["api_key"], base_url=config["endpoint_url"])
+)
+
+
+@cl.on_message
+@traceable
+async def on_message(message: cl.Message):
+    # Maintain an array of messages in the user session
+    message_history = cl.user_session.get(
+        "message_history",
+        [
+            {
+                "role": "system",
+                "content": "You are an AI assistant. Please answer the user's questions to the best of your abilities.",
+            }
+        ],
+    )
+
+    # Processing images exclusively
+    images = (
+        [file for file in message.elements if "image" in file.mime]
+        if message.elements
+        else []
+    )
+
+    if images:
+        # Read the first image and encode it to base64
+        with open(images[0].path, "rb") as f:
+            base64_image = base64.b64encode(f.read()).decode("utf-8")
+        message_history.append(
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": (
+                            message.content
+                            if message.content
+                            else "What’s in this image?"
+                        ),
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+                    },
+                ],
+            }
+        )
+    else:
+        message_history.append({"role": "user", "content": message.content})
+
+    response_message = cl.Message(content="")
+    await response_message.send()
+
+    # Pass in the full message history for each request
+    stream = await client.chat.completions.create(
+        messages=message_history, stream=True, **model_kwargs
+    )
+    async for part in stream:
+        if token := part.choices[0].delta.content or "":
+            await response_message.stream_token(token)
+
+    await response_message.update()
+
+    # Record the AI's response in the history
+    message_history.append({"role": "assistant", "content": response_message.content})
+    cl.user_session.set("message_history", message_history)
